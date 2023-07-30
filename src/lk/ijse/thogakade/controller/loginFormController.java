6e977849d58ce0d1023d2d7bb8994c59c8571f02
Imesh <imeshisuranga00@gmail.com>
2023-07-30 22:23:59 +0530

    Created all GUI(CustomerForm,ItemForm,OrderForm,OrderDetailsForm) and added AppInitializer.java class.

--- src/lk/ijse/thogakade/controller/loginFormController.java
+++ src/lk/ijse/thogakade/controller/loginFormController.java
@@ -1,4 +1,14 @@
 package lk.ijse.thogakade.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class loginFormController {
+    public JFXTextField txtEmail;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void registerOnAction(ActionEvent actionEvent) {
+    }
 }
